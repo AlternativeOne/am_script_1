2b10e4400dd973de4df542d7abeb42cc006379fd
AlternativeOne <132107809+AlternativeOne@users.noreply.github.com>
2024-02-04 17:27:44 +0200

    Update script.py

--- script.py
+++ script.py
@@ -28,16 +28,16 @@ try:
       i1 = trBody.index(titleStartTag, i) + len(titleStartTag)
       i2 = trBody.index(titleCloseTag, i1)
       title = trBody[i1:i2]
-      #title=unquote(title)
-      title=title.encode('utf-8')
+      title=unquote(title)
+      title=title.decode("unicode-escape")
       title=title.replace("&#33;", "!").replace("&#39;", "'").replace("&quot;", "\"")
 
       i = trBody.index(msgIdenTag)
       i1 = trBody.index(msgStartTag, i) + len(msgStartTag)
       i2 = trBody.index(msgCloseTag, i1)
       message=trBody[i1:i2]
-      #message=unquote(message)
-      message=message.encode('utf-8')
+      message=unquote(message)
+      message=message.decode("unicode-escape")
       message=message.replace("&#33;", "!").replace("&#39;", "'").replace("&quot;", "\"")
       
       it['nt']=title
